89bc5f10087bf1180e9cb3c304a9ebd95e96206b
aagr29 <63660748+aagr29@users.noreply.github.com>
2021-08-05 11:16:18 +0930

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# import time
+
+# seconds = 5
+
+# while True:
+#     time.sleep(time.time() % seconds)
+#     print("hello world")
+
+count=100
+
+while count!=0:
+    print(count)
+    count=count-1
